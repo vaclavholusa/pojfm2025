e1de0f074e4326a8dcb226fd6ed2e6a8c6be3906
Václav Holuša <vaclav.holusa@itixo.com>
2025-03-20 07:28:07 +0100

    chatbot

---
+++ 1. lekce/Chatbot/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.AI;
+using Microsoft.Extensions.Configuration;
+using OpenAI;
+
+Console.WriteLine("Můj perfektní chatbot");
+
+var configuration = new ConfigurationBuilder()
+                        .AddJsonFile("appsettings.json")
+                        .Build();
+
+var chatClient = new OpenAIClient(configuration["apiKey"])
+                        .AsChatClient(configuration["model"]);
+
+List<ChatMessage> chatHistory = new()
+{
+    new ChatMessage(ChatRole.System, """
+        Sem přijde briefing bota.
+        Může obsahovat mnoho řádků pokynů
+    """)
+};
+
+while (true)
+{
+    Console.WriteLine("Tvůj prompt:");
+    var userPrompt = Console.ReadLine();
+    chatHistory.Add(new ChatMessage(ChatRole.User, userPrompt));
+
+    Console.WriteLine("AI Odpověď:");
+    var response = "";
+    await foreach (var item in
+        chatClient.GetStreamingResponseAsync(chatHistory))
+    {
+        Console.Write(item.Text);
+        response += item.Text;
+    }
+    chatHistory.Add(new ChatMessage(ChatRole.Assistant, response));
+    Console.WriteLine();
+}
---
+++ 1. lekce/Chatbot/README.md
@@ -0,0 +1,6 @@
+# Chatbot
+
+Ukázka jednoduché integrace chatbota od OpenAI do vlastní aplikace.
+
+## Nastavení
+v souboru `appsettings.json` je potřeba vyplnit vlastní apiKey a případně zvolit model oproti kterému chceme chatovat.
